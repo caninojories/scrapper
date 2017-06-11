6e95d4f00567ffc3edc3679617800bdf9ea58e00
Jo-Ries Canino <canino_jories@hotmail.com>
2017-06-11 11:25:20 +0800

    Addintg wait command for casperjs

--- casper.ts
+++ casper.ts
@@ -1,10 +1,12 @@
 let casper = require('casper').create();
 
 casper.start(casper.cli.get('rootUrl'))
-.then(function () {
+casper.waitForSelector(casper.cli.get('input'))
+casper.then(function () {
   return this.click(casper.cli.get('input'));
 })
-.then(function() {
+casper.waitForSelector('body');
+casper.then(function() {
   if (this.currentHTTPStatus === 200) {
     console.log(this.getCurrentUrl());
   } else {
